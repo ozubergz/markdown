c123fdfce95e7f47d5cc7f643dbdebd1f32f7ad1
ozubergs <34900230+ozubergs@users.noreply.github.com>
2018-11-06 21:40:48 -0500

    Updates

---
+++ precache-manifest.bb408058decf17d8e458e19bff0c93df.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "afa668af6f0d1d649688",
+    "url": "/markdown/static/js/runtime~main.afa668af.js"
+  },
+  {
+    "revision": "26463fb3ebe5560267dd",
+    "url": "/markdown/static/js/main.26463fb3.chunk.js"
+  },
+  {
+    "revision": "d17612b8908539c56069",
+    "url": "/markdown/static/js/1.d17612b8.chunk.js"
+  },
+  {
+    "revision": "1f0add19648db55a5a6242b2766f1240",
+    "url": "/markdown/index.html"
+  }
+];
